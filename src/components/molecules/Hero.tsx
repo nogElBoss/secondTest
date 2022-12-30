c9ecd38e1799d418a8bb7382c56135cda977ecd4
Gonçalo Nogueira <gn16102000@gmail.com>
2022-12-30 15:57:00 +0000

    Hero created, images folder created, logos and icons inserted in images folder

---
+++ src/components/molecules/Hero.tsx
@@ -0,0 +1,21 @@
+import { Flex, Heading, Box, Image} from '@chakra-ui/react'
+
+export const Hero = () => (
+  <Flex
+    justifyContent="center"
+    alignItems="center"
+    height="100vh"
+  >
+    <Box m={50} h={300} w={300}>
+      <Image src="/images/logo.png" alt='Dan Abramov' />
+    </Box>
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      color="white"
+    >
+      <Heading fontSize={110} mb={-3}>Gonçalo</Heading>
+      <Heading fontSize={110} mt={-3}>Nogueira</Heading>
+    </Flex>
+  </Flex>
+)
